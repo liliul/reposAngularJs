bfbb6057b1dcd322ecbf58823f515bea9d2ed35a
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-08-04 19:10:24 -0300

    create: pasta component e Card.jsx

---
+++ reactCardsjs/src/component/Card.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+function Card() {
+	
+	return (
+
+		<>
+			<section className="card">
+
+				<div className="avatar">avatar</div>
+
+				<h2 className="name">name</h2>
+
+				<article className="infos">
+					
+				</article>
+			</section>
+		</>
+	)
+}
+
+export default Card;
