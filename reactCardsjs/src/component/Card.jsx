a2ba4fff4603ec35f31e41f7c17e12a95cd92a2e
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-08-04 19:24:14 -0300

    feat: estrutura html

--- reactCardsjs/src/component/Card.jsx
+++ reactCardsjs/src/component/Card.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 
 function Card() {
-	
+	const [apiGithub, setApiGithub] = useState(0);
+
 	return (
 
 		<>
@@ -12,8 +13,18 @@ function Card() {
 				<h2 className="name">name</h2>
 
 				<article className="infos">
-					
+					<div className="group-infos">
+						<h4>Company: <span></span></h4>
+						<h4>Blog: <span></span></h4>
+						<h4>Location: <span></span></h4>
+					</div>
+
+					<div className="bio">bio</div>				
 				</article>
+
+				<footer className="footer">
+					<a href="" className="link-repo">link-repo</a>
+				</footer>
 			</section>
 		</>
 	)
