718c65decf7c1695de5935b74a6245a6fb9f6d24
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-08-04 21:21:05 -0300

    feat: lendo api do github

--- reactCardsjs/src/component/Card.jsx
+++ reactCardsjs/src/component/Card.jsx
@@ -1,26 +1,37 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './Card.css';
 
 function Card() {
-	const [apiGithub, setApiGithub] = useState(0);
+	const [apiGithub, setApiGithub] = useState([]);
+
+	useEffect(() => {
+		fetch('https://api.github.com/users/maykbrito')
+			.then(response => response.json())
+			.then(data => setApiGithub(data))
+	})
 
 	return (
 
 		<>
 			<section className="card">
 
-				<div className="avatar">avatar</div>
+				<div className="avatar">
+					<img className="img-avatar" src={apiGithub.avatar_url} alt="Avatar" />
+				</div>
 
-				<h2 className="name">name</h2>
+				<h2 className="name">{apiGithub.name}</h2>
 
 				<article className="infos">
 					<div className="group-infos">
-						<h4>Company: <span></span></h4>
-						<h4>Blog: <span></span></h4>
-						<h4>Location: <span></span></h4>
+						<h4>Company: <span>{apiGithub.company}</span></h4>
+						<h4>Blog: <span>{apiGithub.blog}</span></h4>
+						<h4>Location: <span>{apiGithub.location}</span></h4>
 					</div>
 
-					<div className="bio">bio</div>				
+					<div className="bio">
+						<h4>Bio:</h4>
+						<p>{apiGithub.bio}</p>
+					</div>				
 				</article>
 
 				<footer className="footer">
