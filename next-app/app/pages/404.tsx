6b15faeab52a95121f7923868a0e87ba61db6ddd
liliul <liliuestudo@gmail.com>
2023-08-13 09:55:21 -0300

    create: pages nextJS 13

---
+++ next-app/app/pages/404.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Custom404() {
+  return (
+      </>
+        <h1>Erro 404</h1>
+        <Link href="/">Voltar para pagina inicial?</Link>
+      </>
+    )
+}
