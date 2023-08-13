b80893e45557fa0a346dd09124ee5c4ec4b96568
liliul <liliuestudo@gmail.com>
2023-08-13 10:11:39 -0300

    add: tag head do nextJS 13

--- next-app/app/about/page.tsx
+++ next-app/app/about/page.tsx
@@ -1,8 +1,12 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default function About() {
     return (
         <>
+            <Head>
+                <title>About page</title>
+            </Head>
             <h1>About</h1>
             <Link href="/">Page Inicial</Link>
         </>
--- next-app/app/content/page.tsx
+++ next-app/app/content/page.tsx
@@ -1,8 +1,13 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 const Content = () => {
     return (
         <>
+            <Head>
+                <title>Content Page</title>
+            </Head>
+
             <h1>Content</h1>
             <Link href="/">Page Inicial</Link>
         </>
