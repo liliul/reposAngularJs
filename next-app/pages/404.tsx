250a2f21b8d2b3cfa11c1f0a5d94813eee372364
liliul <liliuestudo@gmail.com>
2023-08-14 22:38:33 -0300

    fix: remoneando extensões da pasta pages

--- next-app/pages/404.tsx
+++ next-app/pages/404.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link'
 
 export default function Custom404() {
   return (
-      </>
-        <h1>Erro 404</h1>
+      <div>
+        <h2>Erro 404</h2>
         <Link href="/">Voltar para pagina inicial?</Link>
-      </>
+      </div>
     )
 }
--- next-app/pages/500.tsx
+++ next-app/pages/500.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Custom404() {
   return (
-      </>
+      <>
         <h1>Erro 500</h1>
         <Link href="/">Voltar para pagina inicial?</Link>
       </>
