94822242f1edfe3810beced62fa1fb416d662e92
liliul <liliuestudo@gmail.com>
2023-08-16 14:04:06 -0300

    create: counter

---
+++ next-js-12/src/app/conteudo/counter/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { useState } from "react"
+
+export default function Counter() {
+    const [counter, setCounter] = useState(0)
+
+    return (
+        <div className="w-full h-[400px] flex justify-center pt-10">
+            <button onClick={() => setCounter(counter + 1)}
+                className="w-[44px] h-[44px] bg-gray-400 mr-5 border-e-2 border-gray-900"
+            >
+                calc
+            </button>
+
+            <span>{counter}</span>
+        </div>
+    )
+}
