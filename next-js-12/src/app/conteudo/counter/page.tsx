eec1ebcdd1b5648516d1f3daddc6f32487ec4e40
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-08-21 19:11:21 -0300

    style: feat counter menos e mais next-js-12

--- next-js-12/src/app/conteudo/counter/page.tsx
+++ next-js-12/src/app/conteudo/counter/page.tsx
@@ -6,15 +6,15 @@ export default function Counter() {
     const [counter, setCounter] = useState(0)
 
     return (
-        <div className="w-full h-[400px] flex justify-center pt-10">
+        <div className="w-full h-[400px] flex justify-center items-center pt-10">
             <button onClick={() => setCounter(counter + 1)}
-            className="w-[44px] h-[44px] bg-gray-400 mr-5 border-e-2 border-gray-900"
+            className="w-[100px] h-[44px] bg-gray-600 text-bold border-e-2 border-gray-900"
             >mais</button>
 
-            <span>{counter}</span>
+            <span className="mr-5 ml-5">{counter}</span>
 
             <button onClick={() => setCounter(counter - 1)}
-            className="w-[44px] h-[44px] bg-gray-400 mr-5 border-e-2 border-gray-900"
+            className="w-[100px] h-[44px] bg-gray-600 text-bold border-e-2 border-gray-900"
             >Menos</button>
         </div>
     )
