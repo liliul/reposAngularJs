8d57c6d8a8f1743cb55721156867cae72616d27a
liliul <liliuestudo@gmail.com>
2023-08-20 22:37:37 -0300

    create: pages

---
+++ next-js-12/src/pages/404.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function Error404() {
+    return (
+        <>
+            <div className='w-full h-full'>
+                <h1>Error 404</h1>
+            </div>
+        </>
+    )
+}
