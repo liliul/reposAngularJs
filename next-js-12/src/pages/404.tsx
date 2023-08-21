88fa1ff9fdaca03895a1ce75acdba915e651e529
liliul <liliuestudo@gmail.com>
2023-08-20 22:45:45 -0300

    feat: 404.tsx

--- next-js-12/src/pages/404.tsx
+++ next-js-12/src/pages/404.tsx
@@ -4,7 +4,13 @@ export default function Error404() {
     return (
         <>
             <div className='w-full h-full'>
-                <h1>Error 404</h1>
+                <button className='p-5 mt-20 bg-white text-black'>
+                    <Link href='/'>Voltar para o inicio</Link>
+                </button>
+
+                <article className='w-full h-[200px] mt-20 bg-white'>
+                    <h1 className='text-xl text-center text-violet-950'>Error <span className='text-black'>404</span></h1>
+                </article>
             </div>
         </>
     )
