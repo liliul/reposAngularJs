8fbf9fbf4611891e1f4f09f3aec845b221d5d45b
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-08-21 19:03:30 -0300

    feat: counter menos e mais next-js-12

--- next-js-12/src/app/conteudo/counter/page.tsx
+++ next-js-12/src/app/conteudo/counter/page.tsx
@@ -8,12 +8,14 @@ export default function Counter() {
     return (
         <div className="w-full h-[400px] flex justify-center pt-10">
             <button onClick={() => setCounter(counter + 1)}
-                className="w-[44px] h-[44px] bg-gray-400 mr-5 border-e-2 border-gray-900"
-            >
-                calc
-            </button>
+            className="w-[44px] h-[44px] bg-gray-400 mr-5 border-e-2 border-gray-900"
+            >mais</button>
 
             <span>{counter}</span>
+
+            <button onClick={() => setCounter(counter - 1)}
+            className="w-[44px] h-[44px] bg-gray-400 mr-5 border-e-2 border-gray-900"
+            >Menos</button>
         </div>
     )
 }
