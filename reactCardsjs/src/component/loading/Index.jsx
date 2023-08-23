a3a99e887ceacb00e9eb268d36606edc3c228a1d
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-08-23 11:29:41 -0300

    style: loading

--- reactCardsjs/src/component/loading/Index.jsx
+++ reactCardsjs/src/component/loading/Index.jsx
@@ -1,7 +1,9 @@
+import './Index.css';
+
 export default function Loading() {
     return (
         <>
-            <h1>Loading...</h1>
+            <h1 className="load">Loading...</h1>
         </>
     )
 }
