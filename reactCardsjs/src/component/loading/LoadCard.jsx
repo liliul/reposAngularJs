68cd5afef64c21b3addfa46c377829e97ae1c568
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-08-24 13:33:04 -0300

    create: LoadCard.jsx

---
+++ reactCardsjs/src/component/loading/LoadCard.jsx
@@ -0,0 +1,22 @@
+export default function LoadCard() {
+    return (
+        <>
+            <div className="loadCard">
+                <div className="card-avatar"></div>
+
+                <div className="card-name"></div>
+
+                <section className="card-infos">
+                    <div className="card-items"></div>
+                    <div className="card-items"></div>
+                    <div className="card-items"></div>
+                    <div className="card-items"></div>
+                </section>
+
+                <footer className="card-footer">
+                    <div className="foo"></div>
+                </footer>
+            </div>
+        </>
+    )
+}
