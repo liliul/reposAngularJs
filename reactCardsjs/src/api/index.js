ed3f097702e7bd1da2cf764eb81e1f43d18abdd4
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-08-27 19:03:42 -0300

    chore: configurando axios com api do github reactCardsjs

--- reactCardsjs/src/api/index.js
+++ reactCardsjs/src/api/index.js
@@ -1,1 +1,8 @@
 import axios from 'axios';
+
+export async function apiRepos() {
+    const response = await axios.get('https://api.github.com/users/maykbrito/repos');
+    
+    console.log(response)
+}   
+// apiRepos() 
