195352fb2e967e44addf9742b90e9caf26e53e79
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-08-27 19:40:01 -0300

    fix: useEffect reactCardsjs

--- reactCardsjs/src/component/Repos.jsx
+++ reactCardsjs/src/component/Repos.jsx
@@ -2,22 +2,30 @@ import {useState,useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import './Repos.css';
 import Loading from './loading/Index';
+import { apiRepos } from '../api';
 
 function Repos() {
     const [repos, setRepos] = useState([]);
     const [loading, setLoading] = useState(false);
 
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         fetch('https://api.github.com/users/maykbrito/repos')
+    //         .then(req => req.json())
+    //         .then(res => {
+    //             console.log(setRepos(res))
+    //             setLoading(true)
+    //         })
+    //         .catch(error => console.error(error))
+    //     }, 1000)
+    // },[1])
+
     useEffect(() => {
-        setTimeout(() => {
-            fetch('https://api.github.com/users/maykbrito/repos')
-            .then(req => req.json())
-            .then(res => {
-                console.log(setRepos(res))
-                setLoading(true)
-            })
-            .catch(error => console.error(error))
-        }, 1000)
-    },[1])
+        apiRepos.get("").then((response) => {
+            setRepos(response.data)
+            setLoading(true)
+        })
+    }, [])
 
     return (
         <>
