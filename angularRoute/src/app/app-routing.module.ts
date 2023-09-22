cf7c84d54a7379b688d2fc34f6b2e03116135066
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-09-22 18:45:24 -0300

    create: rota para a page principal angularRoute

--- angularRoute/src/app/app-routing.module.ts
+++ angularRoute/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+
 const routes: Routes = [
+  {path: '', component: AppComponent},
   {path: 'home-page', component: HomePageComponent}
 ];
 
