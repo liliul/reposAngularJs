e2dc6b07ab054f1c77b06224b0c9817200c454ce
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-09-24 18:59:36 -0300

    chore: configurando requisição http

--- angularRoute/src/app/app.module.ts
+++ angularRoute/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +22,8 @@ import { RenderJsonComponent } from './components/render-json/render-json.compon
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSlideToggleModule,
-    MatAutocompleteModule
+    MatAutocompleteModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
