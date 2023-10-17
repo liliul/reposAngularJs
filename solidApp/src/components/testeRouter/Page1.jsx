12adc212809d9add4f0a5e07a3eb149e61c1d88e
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-10-17 19:41:49 -0300

    feat: solid-meta no Page1 solidjs

--- solidApp/src/components/testeRouter/Page1.jsx
+++ solidApp/src/components/testeRouter/Page1.jsx
@@ -1,6 +1,16 @@
+import { MetaProvider, Title, Link, Meta } from '@solidjs/meta';
+
 function Page1() {
     return (
         <>
+            <MetaProvider>
+                
+                  <Title>Testando router com solid-meta</Title>
+                  <Link rel="canonical" href="http://solidjs.com/" />
+                  <Meta name="example" content="whatever" />
+                
+            </MetaProvider>
+
             <h1>Testando Router no Solid JS</h1>
         </>
     )
