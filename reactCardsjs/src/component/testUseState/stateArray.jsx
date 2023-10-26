5b0c26a8e8c2ea85dc25e2c0c34734e4557a25d2
liliul <liliuestudo@gmail.com>
2023-10-26 18:37:17 -0300

    feat: usando o useState Array como banco de dados statico

--- reactCardsjs/src/component/testUseState/stateArray.jsx
+++ reactCardsjs/src/component/testUseState/stateArray.jsx
@@ -1,9 +1,35 @@
 import { useState } from 'react';
 
 export default function UseStateArray() {
+	const [useArray, setUseArray] = useState([
+		{
+			id:0,
+			anime:"Naruto Shippuden",
+			status: false
+		},
+		{
+			id:1,
+			anime:"Black anime",
+			status: true
+		},
+		{
+			id:2,
+			anime:"Onepice",
+			status: true
+		},
+		{
+			id:3,
+			anime:"Dragon Ball Z",
+			status: false
+		}
+	]);
 	return (
 		<>
-			
+			{useArray.map((list)=> 
+				<div key={list.id}>
+					<h1>{list.anime}</h1>
+				</div>
+			)}	
 		</>
 	)
 }
