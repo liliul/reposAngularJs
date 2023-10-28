ba55f2ecf54ff40bd55faa911d30d27a72151243
liliul <liliuestudo@gmail.com>
2023-10-27 21:39:35 -0300

    chore: removendo comentarios

--- reactCardsjs/src/App.jsx
+++ reactCardsjs/src/App.jsx
@@ -1,8 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
 import Card from './component/Card';
 import Repos from './component/Repos';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
