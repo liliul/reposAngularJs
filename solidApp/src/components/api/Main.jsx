b157a5e9092ff1360bb9fb25cfa500bd628b2751
liliul <liliuestudo@gmail.com>
2023-10-29 17:05:31 -0300

    feat: componente testando consumo de api no solidjs

---
+++ solidApp/src/components/api/Main.jsx
@@ -0,0 +1,32 @@
+import { createSignal, Show } from "solid-js";
+import { BookList } from "./List";
+import { AddBook } from "./add";
+const initialBooks = [
+  { title: "Code Complete", author: "Steve McConnell" },
+  { title: "The Hobbit", author: "J.R.R. Tolkien" },
+  { title: "Living a Feminist Life", author: "Sarah Ahmed" },
+];
+export function Bookshelf(props) {
+  const [books, setBooks] = createSignal(initialBooks);
+  const [showForm, setShowForm] = createSignal(false);
+  const toggleForm = () => setShowForm(!showForm());
+  return (
+    <div>
+      <h1>{props.name} s Bookshelf</h1>
+      <BookList books={books()} />
+      <Show
+        when={showForm()}
+        fallback={<button onClick={toggleForm}>Add a book</button>}
+      >
+        <AddBook setBooks={setBooks} />
+        <button onClick={toggleForm}>Finished adding books</button>
+      </Show>
+    </div>
+  );
+}
+// function App() {
+//   return (
+//     <Bookshelf name="Solid"/>
+//   );
+// }
+// export default App;
