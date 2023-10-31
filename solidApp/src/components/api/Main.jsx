0e2fdb91d3bf5f682fabd20f21fb3374fb80a9a8
liliul <liliuestudo@gmail.com>
2023-10-30 21:20:21 -0300

    chore: removendo codigo nao usado Main.jsx solidjs

--- solidApp/src/components/api/Main.jsx
+++ solidApp/src/components/api/Main.jsx
@@ -1,15 +1,18 @@
 import { createSignal, Show } from "solid-js";
 import { BookList } from "./List";
 import { AddBook } from "./add";
+
 const initialBooks = [
   { title: "Code Complete", author: "Steve McConnell" },
   { title: "The Hobbit", author: "J.R.R. Tolkien" },
   { title: "Living a Feminist Life", author: "Sarah Ahmed" },
 ];
+
 export function Bookshelf(props) {
   const [books, setBooks] = createSignal(initialBooks);
   const [showForm, setShowForm] = createSignal(false);
   const toggleForm = () => setShowForm(!showForm());
+
   return (
     <div>
       <h1>{props.name} s Bookshelf</h1>
@@ -24,9 +27,3 @@ export function Bookshelf(props) {
     </div>
   );
 }
-// function App() {
-//   return (
-//     <Bookshelf name="Solid"/>
-//   );
-// }
-// export default App;
