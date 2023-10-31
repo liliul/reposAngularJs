bc3593e7b3ae6d982b72d8d9f0c532ebf8c6b0f1
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-10-31 19:48:48 -0300

    feat: contando usando createEffect solidjs

--- solidApp/src/components/CreateEffect.jsx
+++ solidApp/src/components/CreateEffect.jsx
@@ -1,6 +1,19 @@
-import { createEffect } from "solid-js"
+import { createEffect, createSignal } from "solid-js";
 
 export default function TestandoCreateEffect() {
+    const [count, setCount] = createSignal(0);
+
+    const time = setInterval(() => setCount(count() + 1), 2000);
+
+    createEffect(() => {
+        console.log(count());
+
+        if (count() === 5) {
+            clearInterval(time);
+        }
+
+    })
+
     return (
         <>
             <h3>CreateEffect SolidJS</h3>
