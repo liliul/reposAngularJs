c9ac768684eea82d4b288971fdb9ca82e9f9413d
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-10-31 19:35:29 -0300

    create: CreateEffect.jsx

---
+++ solidApp/src/components/CreateEffect.jsx
@@ -0,0 +1,9 @@
+import { createEffect } from "solid-js"
+
+export default function TestandoCreateEffect() {
+    return (
+        <>
+            <h3>CreateEffect SolidJS</h3>
+        </>
+    )
+}
