468d5227a986baec4039e4c191d387c629b9bdd1
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-11-02 11:07:01 -0300

    create: CreateEffect2 solidjs

---
+++ solidApp/src/components/CreateEffect2.jsx
@@ -0,0 +1,11 @@
+import { createEffect } from 'solid-js';
+
+export default function Effect2() {
+	return (
+		<>
+			<div className="props">
+				<button>Update Name</button>
+			</div>
+		</>
+	)
+}
