7e2c86e93cc0f003b05797eb02d5f246a33dc860
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-11-04 18:11:08 -0300

    feat: CounterX solidjs

--- solidApp/src/components/CreateSignal3.jsx
+++ solidApp/src/components/CreateSignal3.jsx
@@ -1,10 +1,20 @@
 import { createSignal } from 'solid-js';
 
 function CounterX({count}) {
+	const [counterX, setCounterX] = createSignal(0);
+	const resProps = count.counterX || 'Iniciar';
+
+	function handleCount() {
+		setCounterX(counterX() * 2);
+	}
 
 	return (
 		<>
-			
+			<div>
+				<p>{ resProps }</p>
+
+				<button onClick={handleCount}>Multiplicar</button>
+			</div>
 		</>
 	);
 }
