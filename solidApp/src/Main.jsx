26001444ac8bd59c46b81e86200c518a4dcab688
liliul <liliuestudo@gmail.com>
2023-11-10 19:20:32 -0300

    feat: Main para agrupar componentes solidjs

---
+++ solidApp/src/Main.jsx
@@ -0,0 +1,87 @@
+import { A, Outlet } from "@solidjs/router";
+
+import MyComponent from './components/Props1';
+import TestandoCreateSignal from './components/CreateSignal';
+import Form from './components/CreateSignal2';
+import Parent from './components/Props2';
+import { Bookshelf } from './components/api/Main';
+import TestandoCreateEffect from './components/CreateEffect';
+import Effect2 from './components/CreateEffect2';
+import CounterX from './components/CreateSignal3';
+import GithubApiUser from './components/fetchApiSolidjs/githubApiUser';
+
+export default function Main() {
+       return (
+        <>
+
+              <div>
+                <MyComponent name="Solid props javascript" />
+              </div>
+
+              <br />
+
+              <div>
+                <TestandoCreateSignal />
+              </div>
+
+              <br />
+
+              <div>
+                <Form />
+              </div>
+
+              <br />
+
+              <div class="props">
+                <Parent />
+              </div>
+
+              <br />
+
+              <Outlet />
+
+              <br />
+
+              <div class="props">
+                <A href="/element">Personagens de Naruto</A>
+              </div>
+
+              <br />
+
+              <div class="props">
+                <A href="/userapi">UserApi</A>
+              </div>
+
+              <br />
+
+              <div class="props">
+                <Bookshelf name="Solid" />
+              </div>
+
+              <br />
+
+              <div class="props">
+                <TestandoCreateEffect />
+              </div>
+
+              <br />
+
+              <div class="props">
+                <Effect2 />
+              </div>
+
+              <br />
+
+              <div class="props">
+                <CounterX count='Multiplicar' />
+              </div>
+
+              <br />
+              
+              <GithubApiUser />
+                
+        </> 
+
+       )
+
+}
