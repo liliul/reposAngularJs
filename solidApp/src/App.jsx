82e029829ba59a36912c7c77fe6d56b7a30fa907
liliul <liliuestudo@gmail.com>
2023-11-11 19:49:48 -0300

    chore: removendo codigo não usado no App.jsx solidjs

--- solidApp/src/App.jsx
+++ solidApp/src/App.jsx
@@ -6,21 +6,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import MainComponents from './Main';
-// import MyComponent from './components/Props1';
-// import TestandoCreateSignal from './components/CreateSignal';
-// import Form from './components/CreateSignal2';
-// import Parent from './components/Props2';
+import Mycount from './components/CreateSignal4';
 
 const UserApi = lazy(() => import('./components/fetchApiSolidjs/FetchUser'));
-
 const Page1 = lazy(() => import('./components/testeRouter/Page1'));
 
-// import { Bookshelf } from './components/api/Main';
-// import TestandoCreateEffect from './components/CreateEffect';
-// import Effect2 from './components/CreateEffect2';
-// import CounterX from './components/CreateSignal3';
-// import GithubApiUser from './components/fetchApiSolidjs/githubApiUser';
-import Mycount from './components/CreateSignal4';
 
 function App() {
   const [count, setCount] = createSignal(0)
@@ -67,72 +57,8 @@ function App() {
       <p class="read-the-docs">
         Click on the Vite and Solid logos to learn more
       </p>
-      <MainComponents />
-      {/*<div>
-        <MyComponent name="Solid props javascript" />
-      </div>
-
-      <br />
-
-      <div>
-        <TestandoCreateSignal />
-      </div>
-
-      <br />
-
-      <div>
-        <Form />
-      </div>
-
-      <br />
-
-      <div class="props">
-        <Parent />
-      </div>
-
-      <br />
-
-      <Outlet />
-
-      <br />
-
-      <div class="props">
-        <A href="/element">Personagens de Naruto</A>
-      </div>
-
-      <br />
-
-      <div class="props">
-        <A href="/userapi">UserApi</A>
-      </div>
-
-      <br />
-
-      <div class="props">
-        <Bookshelf name="Solid" />
-      </div>
-
-      <br />
-
-      <div class="props">
-        <TestandoCreateEffect />
-      </div>
-
-      <br />
-
-      <div class="props">
-        <Effect2 />
-      </div>
-
-      <br />
-
-      <div class="props">
-        <CounterX count='Multiplicar' />
-      </div>
-
-      <br />
       
-      <GithubApiUser />*/}
+      <MainComponents />
 
     </>
   )
