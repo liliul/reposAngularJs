71c6303d8778023d60c58a11c5c8fda194028a83
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-11-13 22:17:00 -0300

    chore: jsdoc no Page1.jsx solidjs

--- solidApp/src/components/testeRouter/Page1.jsx
+++ solidApp/src/components/testeRouter/Page1.jsx
@@ -1,12 +1,24 @@
 import { createSignal, For } from 'solid-js';
 import { MetaProvider, Title, Link, Meta } from '@solidjs/meta';
 
+/**
+ * @description variavel statica que guarda nomes 
+ * @const {array} dbJson
+ */
+
 const dbJson = [
-    {name: "Naruto",lastName: "Uzumaki"},
-    {name: "Itachi",lastName: "Uchiha"},
-    {name: "Kakashi",lastName: "Hatake"},
-    {name: "Sasuke",lastName: "Uchiha"},
+    { name: "Naruto", lastName: "Uzumaki" },
+    { name: "Itachi", lastName: "Uchiha" },
+    { name: "Kakashi", lastName: "Hatake" },
+    { name: "Sasuke", lastName: "Uchiha" },
 ];
+
+/**
+ * @function 
+ * @returns uma lista de nomes e sobrenomes
+ * 
+ * @description componente que renderiza uma lista de nome no html
+ */
 
 function Page1() {
     const [listaNomes, setListaNomes] = createSignal("");
@@ -14,11 +26,11 @@ function Page1() {
     return (
         <>
             <MetaProvider>
-                
-                  <Title>Testando router com solid-meta</Title>
-                  <Link rel="canonical" href="http://solidjs.com/" />
-                  <Meta name="example" content="whatever" />
-                
+
+                <Title>Testando router com solid-meta</Title>
+                <Link rel="canonical" href="http://solidjs.com/" />
+                <Meta name="example" content="whatever" />
+
             </MetaProvider>
 
             <h1>Testando Router no Solid JS</h1>
