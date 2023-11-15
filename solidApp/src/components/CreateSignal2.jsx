5da701d105b18ca8ff55813aa84d2cd7cb5251c6
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-11-15 18:59:05 -0300

    chore: jsdoc no CreateSignal2.jsx solidjs

--- solidApp/src/components/CreateSignal2.jsx
+++ solidApp/src/components/CreateSignal2.jsx
@@ -1,11 +1,19 @@
 import { createSignal } from "solid-js";
 
+/**
+ * @function
+ * @returns ### componente que renderiza as entradas do componente Form no html
+ * */
 const BasicComponent = (props) => {
   const value = props.value || "default";
 
   return <div>{value}</div>;
 };
 
+/**
+ * @function
+ * @returns ### componente Form que printa as entradas do input em tempo real 
+ * */
 export default function Form() {
   const [value, setValue] = createSignal("");
 
