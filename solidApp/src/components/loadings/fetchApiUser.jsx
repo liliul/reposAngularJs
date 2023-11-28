e79f8a7da2cc58c87d296418a31eacb45134ba34
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-11-28 20:25:57 -0300

    update: icons e css no fetchApiUser solidjs

--- solidApp/src/components/loadings/fetchApiUser.jsx
+++ solidApp/src/components/loadings/fetchApiUser.jsx
@@ -1,7 +1,13 @@
+import { AiFillMeh } from 'solid-icons/ai'
+
+import './fetchApiUser.css';
+
 export default function LoadFetchApiUser() {
     return (
         <>
-
+            <div class="div-loading">
+                <AiFillMeh size={25} />
+            </div>
         </>
     )
 }
