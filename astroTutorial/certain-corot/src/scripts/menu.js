83469eb7afb5f4b339de46f4c5d45aa82b4a5e9e
liliul <liliuestudo@gmail.com>
2024-01-15 21:31:14 -0300

    chore: jsdoc no script menu astroBlog

--- astroTutorial/certain-corot/src/scripts/menu.js
+++ astroTutorial/certain-corot/src/scripts/menu.js
@@ -1,3 +1,6 @@
+/**
+ * @description javascript que abre e fecha menu no astroBlog
+ */
 document.querySelector('.hamburguer').addEventListener('click', () => {
     document.querySelector('.links-nav').classList.toggle('expandido');
 });
