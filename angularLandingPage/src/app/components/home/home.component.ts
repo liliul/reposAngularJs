50b39053c31b3443a200cf0cbc6be8d9a6e49e2b
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-01-26 15:50:47 -0300

    add: componente footer no home angularLandingPage

--- angularLandingPage/src/app/components/home/home.component.ts
+++ angularLandingPage/src/app/components/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { ContentComponent } from '../content/content.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     HeaderComponent,
-    ContentComponent
+    ContentComponent,
+    FooterComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
