01d8e8477eec6f40789c027dc97913bed33bf9e2
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-01-26 01:57:23 -0300

    add: componente content e div container no home angularLandingPage

--- angularLandingPage/src/app/components/home/home.component.ts
+++ angularLandingPage/src/app/components/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { ContentComponent } from '../content/content.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    ContentComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
