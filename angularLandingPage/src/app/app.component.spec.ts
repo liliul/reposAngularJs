67782a7d0a63d99d86bf0b4f4f4f9726279cb387
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-01-27 13:09:06 -0300

    fix: test app.component angularLandingPage

--- angularLandingPage/src/app/app.component.spec.ts
+++ angularLandingPage/src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angularLandingPage');
+    expect(compiled.querySelector('small')?.textContent).toContain('Copyright todos os direitos reservados');
   });
 });
