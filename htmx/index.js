5706843edca06d8b98552f37ebe484ab311e173e
liliul <liliuestudo@gmail.com>
2024-02-03 17:31:48 -0300

    feat: primeiros teste com hx-post e hx-get htmx

--- htmx/index.js
+++ htmx/index.js
@@ -3,8 +3,11 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/ini',(res, req) => {
-	res.send('Testando htmx')
+app.post('/ini', (req, res) => {
+	res.send('<button style="color:purple;">Naruto</button>')
+})
+app.get('/info', (req, res) => {
+	res.send('menssagem de Sasuke')
 })
 
 app.listen(8080, () => {
