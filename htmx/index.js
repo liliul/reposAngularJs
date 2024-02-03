f8e4f6c53374cb2fda0c27b0c03789d304e4c24a
liliul <liliuestudo@gmail.com>
2024-02-03 18:14:52 -0300

    feat: teste com hx-target hx-swap htmx

--- htmx/index.js
+++ htmx/index.js
@@ -10,6 +10,27 @@ app.get('/info', (req, res) => {
 	res.send('menssagem de Sasuke')
 })
 
+// novos testes
+app.post('/store', (req, res) => {
+	res.send(`
+		<form hx-post="/store">
+		    <input id="title" name="title" type="text"
+		        hx-post="/validate"
+		        hx-trigger="change"
+		        hx-sync="closest form:abort"
+		    >
+		    <button hx-get="sucessStore" hx-swap="outerHTML">Submit</button>
+		</form>
+	`)
+})
+app.get('/sucessStore', (req, res) => {
+	res.send(`
+		<section hx-target="this" hx-swap="outerHTML">
+			<h1>'Sucesso on Submit en Store'</h1>
+		</section>
+	`)
+})
+
 app.listen(8080, () => {
 	console.log('port: 8080')
 })
