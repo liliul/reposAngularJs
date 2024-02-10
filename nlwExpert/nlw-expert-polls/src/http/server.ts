2170c61078159549409ace2e66721f83ee4f40e4
liliul <liliuestudo@gmail.com>
2024-02-10 02:07:40 -0300

    feat: iniciando  projeto nlw-expert-polls

---
+++ nlwExpert/nlw-expert-polls/src/http/server.ts
@@ -0,0 +1,12 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.post('/polls', (request) => {
+    console.log(request.body)
+    return 'Hello NLW'
+})
+
+app.listen({port: 3333}).then(() => {
+    console.log('HTTP server port:3333')
+})
