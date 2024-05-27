ed91e61e4b36b23b5f0ae0d75c57fcdde718073b
liliul <liliuestudo@gmail.com>
2024-05-27 11:46:55 -0300

    add: feature modal home reactjs reposAngular

--- reactjs/modal/src/App.tsx
+++ reactjs/modal/src/App.tsx
@@ -1,33 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Modal} from './components/modal/modal'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [open, setOpen] = useState<boolean>(false)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>testando modal</h2>
+        <button onClick={() => setOpen(!open)
+        }>Modal</button>
+
+        <Modal 
+          isOpen={open} 
+          setOpen={setOpen} 
+          Title={'Modal com Reactjs e TSX'}
+          Description={'Testando modal com react e typescript'}  
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
