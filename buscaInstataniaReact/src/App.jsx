56f30dab3d59872a682364d96279d9a636ced977
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-06-11 21:39:55 -0300

    create: projeto buscaInstataniaReact

---
+++ buscaInstataniaReact/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='container'>
+
+      </div>
+    </>
+  )
+}
+
+export default App
