214dacc10591b755c35232653fb3f5f9e7d96afd
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-06-17 13:20:11 -0300

    fix: sql no UserController apiNodejs

--- apiNodejs/src/controllers/UserController.js
+++ apiNodejs/src/controllers/UserController.js
@@ -13,7 +13,7 @@ module.exports = {
         const { username, password } = req.body;
 
         const [, affectRows] = await connection.query(`
-                INSET INTO users VALUES (
+                INSERT INTO users VALUES (
                 DEFAULT, 
                 '${username}', 
                 '${password}',
@@ -32,7 +32,7 @@ module.exports = {
         const { username, password } = req.body;
         const [, data] = await connection.query(`
                 SELECT * FROM users 
-                WHERE username='${username}' AND password='${password}' 
+                WHERE username='${username}' AND password='${password}'
                 ORDER BY id DESC LIMIT 1
             `);
 
