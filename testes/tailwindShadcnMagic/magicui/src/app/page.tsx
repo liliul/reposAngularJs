df1167036db14c7176607989a373c3a479732e5f
liliul <liliuestudo@gmail.com>
2024-09-16 21:45:54 -0300

    add: feature meteors magicui

--- testes/tailwindShadcnMagic/magicui/src/app/page.tsx
+++ testes/tailwindShadcnMagic/magicui/src/app/page.tsx
@@ -1,4 +1,5 @@
 import BlurIn from '@/components/magicui/blur-in'
+import Meteors from '@/components/magicui/meteors';
 
 export default function Home() {
   return (
@@ -7,6 +8,22 @@ export default function Home() {
         word="Hello Magic UI"
         className="text-foreground text-4xl font-bold"
       />
+
+      <br />
+
+      <MeteorDemo />
+    </div>
+  );
+}
+
+
+export function MeteorDemo() {
+  return (
+    <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
+      <Meteors number={30} />
+      <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+        Meteors
+      </span>
     </div>
   );
 }
