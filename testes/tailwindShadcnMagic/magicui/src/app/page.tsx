a9a2a9e072f377baa4021072bd238b6c232abff4
liliul <liliuestudo@gmail.com>
2024-09-16 21:09:33 -0300

    add: feature blur-in magicui

--- testes/tailwindShadcnMagic/magicui/src/app/page.tsx
+++ testes/tailwindShadcnMagic/magicui/src/app/page.tsx
@@ -1,8 +1,12 @@
+import BlurIn from '@/components/magicui/blur-in'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex items-center justify-center">
+      <BlurIn 
+        word="Hello Magic UI"
+        className="text-foreground text-4xl font-bold"
+      />
     </div>
   );
 }
