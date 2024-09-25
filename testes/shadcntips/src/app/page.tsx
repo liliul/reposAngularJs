660a18a73d34722526acaead47cfb3c11f8dc88a
liliul <liliuestudo@gmail.com>
2024-09-25 17:58:47 -0300

    feat: Form shadcntips

--- testes/shadcntips/src/app/page.tsx
+++ testes/shadcntips/src/app/page.tsx
@@ -1,16 +1,95 @@
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 // import Image from "next/image";
 
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(1),
+})
+
 export default function Home() {
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      email: '',
+      password: '',
+    }
+  })
+
+  function onSubmmit(data: z.infer<typeof schema>) {
+    console.log(data)
+  }
+
   return (
     <>
       <main className="w-full h-screen flex justify-center items-center">
         <Card className="mx-auto max-w-sm">
           <CardHeader>
             <CardTitle className="text-2xl font-bold">Login</CardTitle>
-
-            <CardDescription>Enter your email below to login to your accont</CardDescription>
+              <CardDescription>Enter your email below to login to your accont</CardDescription>
           </CardHeader>
+          <CardContent>
+                <Form {...form}>
+                  <form onSubmit={form.handleSubmit(onSubmmit)}> 
+                    <div className="flex flex-col gap-3">
+                      <FormField 
+                        control={form.control}
+                        name="email"
+                        render={({field}) => (
+                          <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                              <Input placeholder="Digitar email" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+
+                      <FormField 
+                        control={form.control}
+                        name="password"
+                        render={({field}) => (
+                          <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                              <Input 
+                                placeholder="Sua senha" {...field} 
+                                type="password"
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+
+                      <Link href="/" className="ml-auto inline-block text-sm underline text-muted-foreground">
+                        Forget your password?
+                      </Link>
+
+                      <Button type="submit" className="w-full">
+                        Login
+                      </Button>
+                      <Button variant="outline" type="button" className="w-full">
+                        Login with Google
+                      </Button>
+                    </div>
+                  </form>
+                </Form>
+
+                <p className="text-center mt-4 text-sm text-muted-foreground">
+                  Don&apos;t have an account?{' '}
+                  <Link href="/" className="outline">Sign up</Link>
+                </p>
+              </CardContent>
         </Card>
       </main>
     </>
