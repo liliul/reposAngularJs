112940feb1300a28a22bb005d765f822f39dbf90
liliul <liliuestudo@gmail.com>
2024-09-25 17:12:40 -0300

    add: components do shadcn ui shadcntips

--- testes/shadcntips/src/app/page.tsx
+++ testes/shadcntips/src/app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+// import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      Home
+      <main className="w-full h-screen flex justify-center items-center">
+        <Card className="mx-auto max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-2xl font-bold">Login</CardTitle>
+
+            <CardDescription>Enter your email below to login to your accont</CardDescription>
+          </CardHeader>
+        </Card>
+      </main>
     </>
   );
 }
