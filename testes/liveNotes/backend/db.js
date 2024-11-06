32929455dc6eb371eca10c2d58c0c4a5e90133d9
liliul <liliuestudo@gmail.com>
2024-11-06 20:13:05 -0300

    feat: saveNote getNote markNoteAsOpened deleteExpiredNotes liveNotes

--- testes/liveNotes/backend/db.js
+++ testes/liveNotes/backend/db.js
@@ -6,8 +6,45 @@ db.serialize(() => {
         CREATE TABLE IF NOT EXISTS notes (
             id TEXT PRIMARY KEY,
             content TEXT,
-            opened_at DATE DEFAULT null,
-            created_at DATE DEFAULT (datetime('now', 'localtime))
-        )
-    `)
-})
+            opened_at DATE DEFAULT NULL,
+            created_at DATE DEFAULT (datetime('now', 'localtime'))
+        )`);
+});
+
+const saveNote = (id, content) => new Promise((resolve, reject) => {
+    db.run(
+        `INSERT INTO notes (id, content) VALUES (?, ?)`, 
+        [id, content], 
+        (err) => err ? reject(err) : resolve()
+    );
+});
+
+const getNote = (id) => new Promise((resolve, reject) => 
+    db.get(`
+        SELECT * FROM notes WHERE id = ?
+    `, [id], (err, row) => err ? reject(err) : resolve(row))
+
+)
+
+const markNoteAsOpened = (id) => new Promise((resolve, reject) => 
+    db.run(`
+        UPDATE notes SET opened_at = datetime('now', 'localtime') WHERE id = ?
+
+    `, [id], (err) => err ? reject(err) : resolve())
+
+)
+
+const deleteExpiredNotes = () => new Promise((resolve, reject) => 
+    db.run(`
+        DELETE FROM notes
+        WHERE opened_at < datetime('now', 'localtime', '-5 minutes')
+        OR opened_at IS NULL AND created_at < datetime('now', 'localtime', '-7 days')
+    `, (err) => err ? reject(err) : resolve())
+)
+
+module.exports = {
+    saveNote,
+    getNote,
+    markNoteAsOpened,
+    deleteExpiredNotes
+}
--- testes/liveNotes/backend/index.js
+++ testes/liveNotes/backend/index.js
@@ -4,9 +4,11 @@
  * */
 
 const express = require('express');
+const { saveNote, getNote, deleteExpiredNotes, markNoteAsOpened } = require('./db.js')
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
@@ -15,6 +17,40 @@ app.get('/', (req, res) => {
 app.get('/note/:id', (req, res) => {
 	res.sendFile(__dirname + '/public/note.html')
 })
+
+app.post('/notes', async (req, res) => {
+	const { content } = req.body
+
+	if (!content) {
+		return res.send('<span>Erro inesperado</span>')
+	}
+
+	const id = crypto.randomUUID()
+	await saveNote(id, content)
+	res.send(`
+		p>
+			Compartilhar a sua nota atraves do link
+			<br>
+			<span>${req.headers.origin}/note/${id}</span>
+		</p>
+	`)
+})
+
+app.get('share/:id', async (req, res) => {
+	await deleteExpiredNotes()
+
+	const { id } = req.params
+	const note = await getNote(id)
+	if (!note) {
+		return res.send('<span class="error">Esta mensagem não existe mais!</span>')
+	}
+
+	if (!note.opened_at) {
+		await markNoteAsOpened(id)
+	}
+
+	res.send(note.content)
+ })
 
 const porta = 3000;
 app.listen(porta, () => {
