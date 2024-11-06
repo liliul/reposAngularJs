8a902a6725c92c4cf354fa092b76b513963211a2
liliul <liliuestudo@gmail.com>
2024-11-05 22:05:16 -0300

    update: backend liveNotes

--- testes/liveNotes/backend/index.js
+++ testes/liveNotes/backend/index.js
@@ -1,8 +1,19 @@
+/**
+ * link https://www.youtube.com/watch?v=ixKkaWClWBE
+ * 
+ * */
+
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-	res.send('backend')
+	res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/note/:id', (req, res) => {
+	res.sendFile(__dirname + '/public/note.html')
 })
 
 const porta = 3000;
