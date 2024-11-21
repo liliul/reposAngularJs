887999c9f99c1fcd380a9cf8994019f2d31d8fe0
liliul <liliuestudo@gmail.com>
2024-11-21 18:36:25 -0300

    update: useContext reactTest1

--- reactjs/reactTest1/src/components/useContext/MyComponent.jsx
+++ reactjs/reactTest1/src/components/useContext/MyComponent.jsx
@@ -3,11 +3,12 @@ import { MyContext } from './MyProvider';
 
 export const MyComponent = () => {
   const { value, setValue } = useContext(MyContext); 
-
+  console.log(value)
   return (
     <div>
       <p>{value}</p>
-      <button onClick={() => setValue('Novo valor!')}>Alterar Valor</button>
+      <button onClick={() => setValue('novo value Som goku z')}>Alterar Valor</button>
+      {/* <button onClick={() => setValue(value + 1)}>Alterar Valor</button> */}
     </div>
   );
 };
--- reactjs/reactTest1/src/components/useContext/MyProvider.jsx
+++ reactjs/reactTest1/src/components/useContext/MyProvider.jsx
@@ -3,7 +3,9 @@ import React, { createContext, useState } from 'react';
 export const MyContext = createContext();
 
 const MyProvider = ({ children }) => {
-  const [value, setValue] = useState('Texto do Contexto');
+  const [value, setValue] = useState('Naruto provider');
+  // const [value, setValue] = useState(0);
+
 
   return (
     <MyContext.Provider value={{ value, setValue }}>
