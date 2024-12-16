ccc052504bf77d05f94495f1bb8c600c1208a6ad
liliul <liliuestudo@gmail.com>
2024-12-15 22:25:28 -0300

    update: no componente buttonCustom reactTest1

--- reactjs/reactTest1/src/components/buttonCustom/index.jsx
+++ reactjs/reactTest1/src/components/buttonCustom/index.jsx
@@ -1,19 +1,22 @@
+import './index.css';
+
 export function ButtonCustom() {
     return (
         <>
             <section className="c-button-custom">
                 <div className="c-content-custom">
                     <article className="text-custom">
-                        <h1>Texto Customizando</h1>
+                        <h2>Texto Customizando</h2>
 
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia</p>
 
                         <p>natus minus, culpa, ullam aperiam rerum est voluptas ducimus libero corrupti at. Molestiae officiis nihil odio sint veniam sit iure quaerat?</p>
                     </article>
 
-                    <div className="button-custom">
-                        <button>Dark green</button>
-                        <button>Light blue</button>
+                    <div className='div-button-custom'>
+                        <button className="button-custom">Dark green</button>
+                        
+                        <button className="button-custom">Light blue</button>
                     </div>
                 </div>
             </section>
