775c2eb99d3981bca27e97a9a7a79ca6cfcc353c
liliul <liliuestudo@gmail.com>
2024-12-15 22:12:49 -0300

    create: componente buttonCustom reactTest1

---
+++ reactjs/reactTest1/src/components/buttonCustom/index.jsx
@@ -0,0 +1,22 @@
+export function ButtonCustom() {
+    return (
+        <>
+            <section className="c-button-custom">
+                <div className="c-content-custom">
+                    <article className="text-custom">
+                        <h1>Texto Customizando</h1>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia</p>
+
+                        <p>natus minus, culpa, ullam aperiam rerum est voluptas ducimus libero corrupti at. Molestiae officiis nihil odio sint veniam sit iure quaerat?</p>
+                    </article>
+
+                    <div className="button-custom">
+                        <button>Dark green</button>
+                        <button>Light blue</button>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
