053efbb56d451ad0b7331539fc506e4bafa95f67
liliul <liliuestudo@gmail.com>
2024-12-19 21:52:50 -0300

    fix: desativando o tailwindcss reactTest1

--- reactjs/reactTest1/src/components/routesComponents/home.jsx
+++ reactjs/reactTest1/src/components/routesComponents/home.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom"
 
 export const Home = () => {
     return (
-        <div>
+        <div className="bg-slate-600">
             <h2>Home Page</h2>
 
-            <Link to="/login">Login</Link>
+            <Link to="/login" className="text-blue-800">Login</Link>
         </div>
     )
 }
