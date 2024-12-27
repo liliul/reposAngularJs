359a92beb089057439a0159e5ae949923f9d7454
liliul <liliuestudo@gmail.com>
2024-12-27 19:28:28 -0300

    fix: mostra componente SingOutButton somente logado reactAuthContextApi

--- reactjs/reactAuthContextApi/frontend/src/components/LayoutComponents/index.jsx
+++ reactjs/reactAuthContextApi/frontend/src/components/LayoutComponents/index.jsx
@@ -1,13 +1,17 @@
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+
 import SingOutButton from "../../components/singOutButton";
 
 import "./styles.css";
 
 export const LayoutComponents = (props) => {
+  const { signed } = useContext(AuthContext)
   return (
     <div className="container">
       <div className="container-login">
-        <SingOutButton />
-        
+        {signed === true ? <SingOutButton /> : ''}
+
         <div className="wrap-login">{props.children}</div>
       </div>
     </div>
