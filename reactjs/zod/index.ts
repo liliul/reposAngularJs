c33e934b2251d16cefbbc16610ff9d78ad0ed2be
liliul <liliuestudo@gmail.com>
2025-01-08 22:16:42 -0300

    feat: index2 zod

--- reactjs/zod/index.ts
+++ reactjs/zod/index.ts
@@ -1,16 +1,3 @@
+// codigos na pasta lib
 import { z } from 'zod'
 
-const userSchema = z.object({
-    name: z.string().min(2),
-    workDays: z.array(
-        z.string().transform(day => day.slice(0, 3).toLowerCase())
-    )
-})
-
-const user = {
-    name: "Er",
-    workDays: ["SegUnda", "TerçA", "quaRTA", "QUINtA", "SeXtA"]
-}
-
-const { success, data } = userSchema.safeParse(user)
-console.log(data);
