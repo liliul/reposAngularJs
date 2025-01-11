31dd899efa15fadca4ffef70969e6fe9561098ab
liliul <liliuestudo@gmail.com>
2025-01-10 21:29:35 -0300

    chore: feature counterslice no configureStore reactTailwindcss

--- reactjs/reactTailwindcss/src/redux/store1/store.js
+++ reactjs/reactTailwindcss/src/redux/store1/store.js
@@ -2,5 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import counterslice from '../createSlice1/counterslice'
 
 export default configureStore({
-  reducer: {}
+  reducer: {
+    counter: counterslice
+  }
 })
