cec1336b04c6ae154927c69d470e6ad61b35545f
liliul <liliuestudo@gmail.com>
2025-02-01 22:07:37 -0300

    fix: cors express reactBackend

--- reactjs/reactBackend/backend/src/server.js
+++ reactjs/reactBackend/backend/src/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 const PORT = 8081;
 
 app.use(express.json());
+app.use(cors(
+  {
+    origin: 'http://localhost:8080',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+  }
+));
 app.use('/api', userRoutes);
 
 app.listen(PORT, () => {
