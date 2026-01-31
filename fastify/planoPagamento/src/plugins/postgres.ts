07134cacaac9106b3f42370c1f1ca0bd4785be85
liliul <liliuestudo@gmail.com>
2026-01-30 21:32:27 -0300

    chore: conexao com postgres planoPagamento

---
+++ fastify/planoPagamento/src/plugins/postgres.ts
@@ -0,0 +1,8 @@
+import fp from 'fastify-plugin'
+import postgres from '@fastify/postgres'
+
+export default fp(async (app) => {
+  app.register(postgres, {
+    connectionString: process.env.DATABASE_URL
+  })
+})
